19bdaf275cd4a7b97cb75f6134bd79c5a767bab7
Your Name <you@example.com>
2023-04-17 22:55:05 +0300

    modify ping-pong example

--- examples/async-network-ping-pong/src/main.rs
+++ examples/async-network-ping-pong/src/main.rs
@@ -9,8 +9,10 @@ use dslab_core::{Id, Simulation, SimulationContext};
 use dslab_network::topology_model::TopologyNetwork;
 use dslab_network::{constant_bandwidth_model::ConstantBandwidthNetwork, network::Network};
 use env_logger::Builder;
-use process::Process;
+use process::NetworkProcess;
 use sugars::{rc, refcell};
+
+use crate::process::Process;
 
 mod process;
 
@@ -18,59 +20,123 @@ mod process;
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Number of processes (>= 2)
-    #[clap(long, default_value_t = 5)]
+    #[clap(long, default_value_t = 2)]
     proc_count: u32,
+
+    /// Number of process peers (>= 1)
+    #[clap(long, default_value_t = 1)]
+    peer_count: usize,
+
+    /// Asymmetric mode
+    #[clap(long)]
+    asymmetric: bool,
+
+    /// Random delay
+    #[clap(long)]
+    rand_delay: bool,
+
+    /// Use network
+    #[clap(long)]
+    use_network: bool,
+
+    /// Number of iterations (>= 1)
+    #[clap(long, default_value_t = 1)]
+    iterations: u32,
 }
 
 fn main() {
     let args = Args::parse();
     let proc_count = args.proc_count;
+    let iterations = args.iterations;
 
     Builder::from_default_env()
         .format(|buf, record| writeln!(buf, "{}", record.args()))
         .init();
-    // env_logger::init();
 
-    let mut simulation = Simulation::new(42);
+    let mut sim = Simulation::new(42);
+    let mut root = sim.create_context("root");
 
-    let network_model = rc!(refcell!(ConstantBandwidthNetwork::new(1000., 0.001)));
-    let network = rc!(refcell!(Network::new(network_model, simulation.create_context("net"))));
-    simulation.add_handler("net", network.clone());
+    let network_opt = if args.use_network {
+        let network_model = rc!(refcell!(ConstantBandwidthNetwork::new(1000., 0.001)));
+        let network = rc!(refcell!(Network::new(network_model, sim.create_context("net"))));
+        sim.add_handler("net", network.clone());
+        network.borrow_mut().add_node("host1", 1000., 0.);
+        network.borrow_mut().add_node("host2", 1000., 0.);
+        Some(network)
+    } else {
+        None
+    };
 
-    let network_nodes = vec!["host1", "host2", "host3"];
-    let bandwidth = vec![200., 300., 500.];
-    let latency = vec![0., 0., 0.];
+    let mut net_processes = Vec::new();
+    let mut processes = Vec::new();
 
-    for i in 0..network_nodes.len() {
-        network
-            .borrow_mut()
-            .add_node(network_nodes[i], bandwidth[i], latency[i]);
-    }
-
-    let mut ctxs = Vec::new();
-
-    let mut network_node_id = 0;
+    let id_offset = args.use_network as u32;
 
     for i in 1..=proc_count {
+        let proc_id = i + id_offset;
         let proc_name = format!("proc{}", i);
-
-        let ctx = simulation.create_context(proc_name);
-
-        network
-            .borrow_mut()
-            .set_location(ctx.id(), network_nodes[network_node_id]);
-
-        ctxs.push(ctx);
-
-        network_node_id = (network_node_id + 1) % network_nodes.len();
+        let mut peers = BTreeSet::new();
+        if args.peer_count == 1 {
+            let peer_id = (i % proc_count) + 1 + id_offset;
+            peers.insert(peer_id);
+        } else {
+            while peers.len() < args.peer_count {
+                let peer_id = root.gen_range(1..=proc_count) + id_offset;
+                if peer_id != proc_id {
+                    peers.insert(peer_id);
+                }
+            }
+        }
+        let is_pinger = !args.asymmetric || i % 2 == 1;
+        if let Some(ref network) = network_opt {
+            let proc = NetworkProcess::new(
+                rc!(refcell!(Vec::from_iter(peers))),
+                is_pinger,
+                iterations,
+                network.clone(),
+                sim.create_context(&proc_name),
+            );
+            sim.add_handler(&proc_name, rc!(refcell!(proc.clone())));
+            let host = format!("host{}", 2 - i % 2);
+            network.borrow_mut().set_location(proc_id, &host);
+            net_processes.push(proc);
+        } else {
+            let proc = Process::new(
+                rc!(refcell!(Vec::from_iter(peers))),
+                is_pinger,
+                args.rand_delay,
+                iterations,
+                sim.create_context(&proc_name),
+            );
+            sim.add_handler(&proc_name, rc!(refcell!(proc.clone())));
+            processes.push(proc);
+        }
     }
 
-    let peers: Vec<Id> = ctxs.iter().map(|ctx| ctx.id()).collect();
-
-    for ctx in ctxs {
-        let proc = Process::new(peers.clone(), network.clone(), ctx);
-        simulation.spawn(process::start_pinger_vm(proc));
+    if args.use_network {
+        for proc in net_processes {
+            sim.spawn(process::start_network_process(proc));
+        }
+    } else {
+        for proc in processes {
+            sim.spawn(process::start_process(proc));
+        }
     }
 
-    simulation.step_until_no_events();
+    let t = Instant::now();
+    sim.step_until_no_events();
+
+    let elapsed = t.elapsed().as_secs_f64();
+    println!(
+        "Processed {} iterations in {:.2?}s ({:.2} iter/s)",
+        iterations,
+        elapsed,
+        iterations as f64 / elapsed
+    );
+    println!(
+        "Processed {} events in {:.2?}s ({:.0} events/s)",
+        sim.event_count(),
+        elapsed,
+        sim.event_count() as f64 / elapsed
+    );
 }
